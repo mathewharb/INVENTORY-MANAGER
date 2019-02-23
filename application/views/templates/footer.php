52f7230d643097f5e2a698e2ef71b527166cda4d
Mathew Harb <30761383+mathewharb@users.noreply.github.com>
2019-02-23 08:17:20 -0800

    Add files via upload

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Mathew</b> Harb
+    </div>
+    <strong>Copyright &copy; <?php echo date('Y') ?>. </strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
